7f91ecd6b06cd6dafe9286c40b803e1f30660c14
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-03-28 08:02:40 +0500

    add skills section to Resume component with tabs for experience, education, skills, and about

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -1,62 +1,54 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
 import { cn } from "../../lib/utils";
 
-function Tabs({
-  className,
-  ...props
-}) {
+function Tabs({ className, ...props }) {
   return (
     <TabsPrimitive.Root
       data-slot="tabs"
       className={cn("flex flex-col gap-2", className)}
-      {...props} />
+      {...props}
+    />
   );
 }
 
-function TabsList({
-  className,
-  ...props
-}) {
+function TabsList({ className, ...props }) {
   return (
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-1",
+        "inline-flex h-auto rounded-md p-1 text-primary ",
         className
       )}
-      {...props} />
+      {...props}
+    />
   );
 }
 
-function TabsTrigger({
-  className,
-  ...props
-}) {
+function TabsTrigger({ className, ...props }) {
   return (
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-        "data-[state=active]:bg-background data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring inline-flex flex-1 items-center justify-center gap-1.5 rounded-md px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "inline-flex items-center w-full bg-[#27272c] justify-center whitespace-nowrap text-white rounded-lg p-3 text-balance font-medium ring-offset-white transition-all desabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#00ff99] data-[state=active]:text-[#1c1c22] data-[state=active]:font-bold data-[state=active]:shadow-sm",
         className
       )}
-      {...props} />
+      {...props}
+    />
   );
 }
 
-function TabsContent({
-  className,
-  ...props
-}) {
+function TabsContent({ className, ...props }) {
   return (
     <TabsPrimitive.Content
       data-slot="tabs-content"
-      className={cn("flex-1 outline-none", className)}
-      {...props} />
+      className={cn("flex-1 outline-none min-h-[480px]", className)}
+      {...props}
+    />
   );
 }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
