65bfb8478d602bbc188a9e72036ff695a3a4ec5a
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-06-21 19:26:22 +0500

    fix: update social media links for accuracy

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 import { FaGithub, FaLinkedin, FaTelegram, FaYoutube } from "react-icons/fa";
 
 const socialIcons = [
-  { icon: <FaGithub />, path: "https://github.com/ozodbekw" },
+  { icon: <FaGithub />, path: "https://github.com/ozodbekDv" },
   {
     icon: <FaLinkedin />,
-    path: "https://www.linkedin.com/in/ozodbek-tursunaliyev",
+    path: "https://www.linkedin.com/in/ozodbek-tursunaliyev-6b89sd22/",
   },
   { icon: <FaTelegram />, path: "https://www.t.me/ozodbek_dv" },
   { icon: <FaYoutube />, path: "https://www.youtube.com/@ozodbek-in-web" },
